a6a8e7facd3eb3161c54c27e5aa1618f38310270
harjas <harjassidhu1@gmail.com>
2024-07-14 09:39:44 +0530

    More Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterSectionComponent } from './components/footer-section/footer-section.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavBarComponent],
+    imports: [RouterOutlet, NavBarComponent, FooterSectionComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
     title = 'RADR';
-    
+
 }
