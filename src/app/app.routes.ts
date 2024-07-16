7cdc90f04afb850206b6736ae51d63e927c34033
GopalChaudh <kc7198931@gmail.com>
2024-07-16 12:10:54 +0530

    fixed effects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MemoriesComponent } from './pages/memories/memories.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'auth', component: AuthComponent}
+    {path: 'auth', component: AuthComponent},
+    {path: 'memories', component: MemoriesComponent},
+    {path: 'events', component: EventsComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutComponent}
 ];
