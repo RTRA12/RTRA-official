40cabfdcf4bb4bcd63837f769e685568b6cd9aa6
harjas <harjassidhu1@gmail.com>
2024-07-18 13:51:35 +0530

    minor changes and finetuning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AboutComponent } from './pages/about/about.component';
-import { MemoriesComponent } from './pages/memories/memories.component';
 import { EventsComponent } from './pages/events/events.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
@@ -10,7 +9,6 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'auth', component: AuthComponent},
-    {path: 'memories', component: MemoriesComponent},
     {path: 'events', component: EventsComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'about', component: AboutComponent},
