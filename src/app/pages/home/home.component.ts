29858e599319e3e4ef6e2807f283fab35f438d42
GopalChaudh <kc7198931@gmail.com>
2024-07-21 08:23:53 +0530

    card fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -38,69 +38,6 @@ export class HomeComponent {
             location: 'Location 3',
             tag: 'Entertainment',
             timings: '06:00 PM - 08:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 4',
-            description: 'This is the fourth event',
-            date: '2021-01-04',
-            location: 'Location 4',
-            tag: 'Education',
-            timings: '10:00 AM - 12:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 5',
-            description: 'This is the fifth event',
-            date: '2021-01-05',
-            location: 'Location 5',
-            tag: 'Talk',
-            timings: '02:00 PM - 04:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 6',
-            description: 'This is the sixth event',
-            date: '2021-01-06',
-            location: 'Location 6',
-            tag: 'Entertainment',
-            timings: '06:00 PM - 08:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 7',
-            description: 'This is the seventh event',
-            date: '2021-01-07',
-            location: 'Location 7',
-            tag: 'Education',
-            timings: '10:00 AM - 12:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 8',
-            description: 'This is the eighth event',
-            date: '2021-01-08',
-            location: 'Location 8',
-            tag: 'Competition',
-            timings: '02:00 PM - 04:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 9',
-            description: 'This is the ninth event',
-            date: '2021-01-09',
-            location: 'Location 9',
-            tag: 'Talk',
-            timings: '06:00 PM - 08:00 PM'
-        },
-        {
-            id:'1234556',
-            title: 'Event 10',
-            description: 'This is the tenth event',
-            date: '2021-01-10',
-            location: 'Location 10',
-            tag: 'Entertainment',
-            timings: '10:00 AM - 12:00 PM'
         }
     ];
 }
