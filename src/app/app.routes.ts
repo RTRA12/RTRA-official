29e3156dd4f52f26c9936ab949023184fe58e3a7
harjas <harjassidhu1@gmail.com>
2024-07-21 13:34:44 +0530

    themed footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
 import { AboutComponent } from './pages/about/about.component';
 import { EventsComponent } from './pages/events/events.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { FullDetailComponent } from './pages/full-detail/full-detail.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'auth', component: AuthComponent},
     {path: 'events', component: EventsComponent},
-    {path: 'contact', component: ContactComponent},
     {path: 'about', component: AboutComponent},
     {path: 'event-detail/:eventId', component: FullDetailComponent},
     {path: '**', component: ErrorPageComponent}
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,32 +2,31 @@ import { CommonModule } from '@angular/common';
 import { Component, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-interface Navigation{
-  title:string,
-  link:string
+interface Navigation {
+    title: string;
+    link: string;
 }
 
 @Component({
     selector: 'app-nav-bar',
     standalone: true,
-    imports: [CommonModule,RouterLink],
+    imports: [CommonModule, RouterLink],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
     isManueOpened: Boolean = false;
     theme: String = 'dark';
-    navigations : Array<Navigation>= [
-      {title:'About',link:'/about'},
-      {title:'Events',link:'/events'},
-      {title:'Contact',link:'/contact'}
+    navigations: Array<Navigation> = [
+        { title: 'Home', link: '/' },
+        { title: 'About', link: '/about' },
+        { title: 'Events', link: '/events' },
     ];
-    constructor(private document: ElementRef) {
-    }
+    constructor(private document: ElementRef) {}
 
     changeTheme() {
         this.theme = this.theme === 'dark' ? 'light' : 'dark';
-        if(this.theme === 'dark') {
+        if (this.theme === 'dark') {
             document.body.classList.remove('light');
         } else {
             document.body.classList.add('light');
@@ -35,16 +34,17 @@ export class NavBarComponent {
     }
 
     ngOnInit() {
-        let themeIcons: HTMLElement[] = this.document.nativeElement.querySelectorAll('.themeChanger');
-        for(let j = 0; j < themeIcons.length; j++) {
+        let themeIcons: HTMLElement[] =
+            this.document.nativeElement.querySelectorAll('.themeChanger');
+        for (let j = 0; j < themeIcons.length; j++) {
             themeIcons[j].addEventListener('click', () => {
-                if(this.theme === 'dark') {
-                    for(let i = 0; i < themeIcons.length; i++) {
+                if (this.theme === 'dark') {
+                    for (let i = 0; i < themeIcons.length; i++) {
                         themeIcons[i].classList.remove('fa-sun');
                         themeIcons[i].classList.add('fa-moon');
                     }
                 } else {
-                    for(let i = 0; i < themeIcons.length; i++) {
+                    for (let i = 0; i < themeIcons.length; i++) {
                         themeIcons[i].classList.remove('fa-moon');
                         themeIcons[i].classList.add('fa-sun');
                     }
@@ -54,4 +54,3 @@ export class NavBarComponent {
         }
     }
 }
-
